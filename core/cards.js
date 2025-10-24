2281ce7099b58885177f99c8ae8671a24cc3c531
Logan <logankeehle8@gmail.com>
2025-10-23 22:49:57 -0400

    push of happiness

--- core/cards.js
+++ core/cards.js
@@ -13,3 +13,17 @@ units[`streetSweeper`] = {
 		attackFrames: [],
 	},
 };
+
+units[`roomba`] = {
+	cost: 300,
+	health: 250,
+	attackInterval: 0.25,
+	attackMelee: 3,
+	attackRanged: 0,
+	attackSplash: 0,
+	images: {
+		idleFrames: [],
+		movementFrames: [],
+		attackFrames: [],
+	},
+};
