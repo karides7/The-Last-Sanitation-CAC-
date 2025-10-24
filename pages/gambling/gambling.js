78cea57488a1d422c3568eb1030e7d2dad74af62
Palmtreelove <nynewboy2016@gmail.com>
2025-10-24 18:44:10 -0400

    gamble update

--- pages/gambling/gambling.js
+++ pages/gambling/gambling.js
@@ -1,5 +1,5 @@
 let cheapButton = document.getElementById(`cheap`);
-let expensiveButton = document.getElementById(`cheap`);
+let expensiveButton = document.getElementById(`expensive`);
 
 let gamblingAnimation = document.createElement(`div`);
 gamblingAnimation.style.position = `absolute`;
@@ -12,7 +12,6 @@ document.body.appendChild(gamblingAnimation);
 let fadeContainer;
 fadeContainer = document.createElement(`div`);
 fadeContainer.style.position = `absolute`;
-fadeContainer.style.zIndex = -50;
 fadeContainer.id = `fadeContainer`;
 fadeContainer.style.width = `80%`;
 fadeContainer.style.height = `100%`;
@@ -20,8 +19,18 @@ fadeContainer.style.left = `calc(300px + 5%)`;
 document.body.appendChild(fadeContainer);
 
 cheapButton.addEventListener(`click`, function () {
-  $(`gamblingAnimation`).load(`./core/fade.html`);
+  $(`#fadeContainer`).load(`./core/fade.html`);
   setTimeout(function () {
-    $(`#container`).load(`./pages/ui/gambling/cheap`);
+    $(`#gamblingAnimation`).load(`./pages/ui/gambling/cheap.html`);
+    document.getElementById(`container`).style.display = `none`;
+  }, 500);
+});
+
+expensiveButton.addEventListener(`click`, function () {
+  $(`#fadeContainer`).load(`./core/fade.html`);
+  setTimeout(function () {
+    $(`#gamblingAnimation`).load(`/pages/ui/gambling/expensive.html`);
+    document.getElementById(`gamblingAnimation`).remove();
+    document.getElementById(`container`).style.display = `block`;
   }, 500);
 });
