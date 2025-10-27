ed6917d5a024d5f23b25a31fa979b06fc992944f
Palmer G <nynewboy2016@gmail.com>
2025-10-27 19:08:36 -0400

    gambling so far

--- pages/ui/gambling/cheap.js
+++ pages/ui/gambling/cheap.js
@@ -1,14 +1,23 @@
 let gumballImage = document.getElementById("gumball");
 
 gumballImage.addEventListener(`click`, function () {
-  if (gumballImage.src === "/resources/images/gumballs/red gumball.png") {
-    gumballImage.src = "/resources/images/gumballs/red gumball part 2.png";
+  if (gumballImage.src.endsWith("/Resources/images/Gumballs/red gumball.png")) {
+    console.log(`#1`)
+    gumballImage.src = "/Resources/images/Gumballs/red gumball part 2.png";
   }
-  if (
-    (gumballImage.src = "/resources/images/gumballs/red gumball part 2.png")
+  else if (
+    (gumballImage.src.endsWith("/Resources/images/Gumballs/red gumball part 2.png"))
   ) {
-    gumballImage.src = "/resources/images/gumballs/red gumball part 3.png";
+    console.log(`#1`)
+    gumballImage.src = "/Resources/images/Gumballs/red gumball part 3.png";
+    
   } else {
-    gumballImage.src = "/resources/images/gumballs/red gumball part 3.png";
+    console.log(`gone`)
+    gumballImage.remove();
   }
 });
+
+//src names
+// Resources\images\Gumballs\red gumball.png
+// Resources\images\Gumballs\red gumball part 2.png
+// Resources\images\Gumballs\red gumball part 3.png
--- pages/ui/gambling/expensive.js
+++ pages/ui/gambling/expensive.js
@@ -1,14 +1,16 @@
 let gumballImage = document.getElementById("gumball");
 
 gumballImage.addEventListener(`click`, function () {
-  if (gumballImage.src === "/resources/images/gumballs/red gumball.png") {
+  if (gumballImage.src.endsWith("/resources/images/gumballs/red gumball.png")) {
     gumballImage.src = "/resources/images/gumballs/red gumball part 2.png";
+    
   }
-  if (
-    (gumballImage.src = "/resources/images/gumballs/red gumball part 2.png")
+  else if (
+    (gumballImage.src.endsWith("/resources/images/gumballs/red gumball part 2.png"))
   ) {
     gumballImage.src = "/resources/images/gumballs/red gumball part 3.png";
+    
   } else {
-    gumballImage.src = "/resources/images/gumballs/red gumball part 3.png";
+    gumballImage.remove();
   }
 });
