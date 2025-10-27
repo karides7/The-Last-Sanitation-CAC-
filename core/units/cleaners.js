c00a9e1f088e2fac055a6ce28f9af4bbe53a0163
Logan <logankeehle8@gmail.com>
2025-10-27 19:09:24 -0400

    skibidi slicers

--- core/units/cleaners.js
+++ core/units/cleaners.js
@@ -4,23 +4,27 @@ import { trashInstances } from "./trash";
 export const cleanerInstances = [];
 
 class Cleaner {
-  constructor(cleanerType) {
-    self.cleanerType = cleanerType;
-    self.data = units[cleanerType];
-    self.position = 100; // in percentage lol
-    self.speedMultiplier = 1;
-  }
-  stun() {
-    // temporarily delays movement whilst in combat
-    self.speedMultiplier = 0.5;
-    setTimeout(function () {
-      self.stunned = false;
-    }, 500);
-  }
-  step(delta) {
-    self.position -= speed * delta;
-    for (let trashMonster of trashInstances) {
-      // get the monsters distance and stats
-    }
-  }
+	constructor(cleanerType) {
+		self.cleanerType = cleanerType;
+		self.data = units[cleanerType];
+		self.position = 100; // in percentage
+		self.speedMultiplier = 1;
+
+		self.container = document.createElement(`div`);
+		self.container.style.height = `20%`;
+	}
+	stun() {
+		self.speedMultiplier = 0.5;
+		setTimeout(function () {
+			self.stunned = false;
+		}, 500);
+	}
+	step(delta) {
+		self.position -= speed * delta;
+		for (let trashMonster of trashInstances) {
+			// get the monsters distance and stats
+		}
+	}
 }
+
+let cleaner = new Cleaner();
