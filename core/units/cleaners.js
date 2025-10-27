88a49eefb530e390d21b9ddfe164a735ac319556
karides347 <logankeehle8@gmail.com>
2025-10-27 17:25:22 -0400

    pushing the classes

--- core/units/cleaners.js
+++ core/units/cleaners.js
@@ -1,6 +1,26 @@
+import { units } from "../cards";
+import { trashInstances } from "./trash";
+
+export const cleanerInstances = [];
+
 class Cleaner {
-	constructor(cleanerType) {
-		self.cleanerType = cleanerType;
-		self.data = `./pages/`;
-	}
+  constructor(cleanerType) {
+    self.cleanerType = cleanerType;
+    self.data = units[cleanerType];
+    self.position = 100; // in percentage lol
+    self.speedMultiplier = 1;
+  }
+  stun() {
+    // temporarily delays movement whilst in combat
+    self.speedMultiplier = 0.5;
+    setTimeout(function () {
+      self.stunned = false;
+    }, 500);
+  }
+  step(delta) {
+    self.position -= speed * delta;
+    for (let trashMonster of trashInstances) {
+      // get the monsters distance and stats
+    }
+  }
 }
