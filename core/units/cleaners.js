d28f525525e712a5ffce948c182e44d5b21e6ced
Logan <logankeehle8@gmail.com>
2025-10-28 15:24:37 -0400

    holeh moleh it moves

--- core/units/cleaners.js
+++ core/units/cleaners.js
@@ -1,36 +1,30 @@
 import { units } from "../cards.js";
-import { trashInstances } from "./trash.js";
 
 export const cleanerInstances = [];
 
 export class Cleaner {
-  constructor(cleanerType) {
-    this.cleanerType = cleanerType;
-    this.data = units[cleanerType];
-    this.position = 100; // in percentage
-    this.speedMultiplier = 1;
+	constructor(cleanerType) {
+		this.cleanerType = cleanerType;
+		this.data = units[cleanerType];
+		console.log(this.data);
+		this.position = 100; // in percentage
+		this.speedMultiplier = 1;
 
-    this.container = document.createElement(`div`);
-    this.container.className = `${cleanerType}`;
-    this.container.style.position = `absolute`;
+		this.container = document.createElement(`div`);
+		this.container.className = `${cleanerType}`;
+		this.container.style.position = `absolute`;
 
-    this.container.style.height = `20%`;
-    this.container.style.width = `20%`;
-    this.container.style.top = `53%`;
+		this.container.style.height = `20%`;
+		this.container.style.top = `53%`;
+		this.container.style.left = `80%`;
+		this.container.style.backgroundColor = `blue`;
+		this.container.style.aspectRatio = `.4`;
 
-    this.container.style.backgroundColor = `blue`;
-    cleanerInstances.push(this);
-  }
-  stun() {
-    this.speedMultiplier = 0.5;
-    setTimeout(function () {
-      this.stunned = false;
-    }, 500);
-  }
-  step(delta) {
-    this.position -= speed * delta;
-    for (let trashMonster of trashInstances) {
-      // get the monsters distance and stats
-    }
-  }
+		cleanerInstances.push(this);
+	}
+	step(delta) {
+		this.position -= this.speedMultiplier * delta * 0.01;
+		console.log(this.position);
+		this.container.style.left = `${0.8 * this.position}%`;
+	}
 }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+import { moveCleaners } from "./core/battle.js";
 import { getData } from "./core/data.js";
-import { updateDelta } from "./core/delta.js";
+import { delta, updateDelta } from "./core/delta.js";
 import { getInput12345 } from "./core/input.js";
 
 getData();
@@ -7,5 +8,6 @@ getData();
 function update(time) {
 	updateDelta(time);
 	window.requestAnimationFrame(update);
+	moveCleaners(delta);
 }
 window.requestAnimationFrame(update);
